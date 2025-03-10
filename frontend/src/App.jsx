cc955c2d0b498ba5056b7d0f63f8fddad17d4830
Alex Murmu <ft.alexrayen@gmial.com>
2025-03-10 13:07:07 +0000

    frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import CreateTodo from './component/CreateTodo'
+import Todo from './component/Todo'
+
+function App() {
+  return (
+    <div>
+      <CreateTodo />
+      <Todo />
+    </div>
+  )
+}
+
+export default App
---
+++ frontend/src/component/Todo.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Todo() {
+  return (
+    <div>
+        <h1>Go to gym</h1>
+        <h2>Go to gym at 12 AM </h2>
+        <button>Mark as completed</button>
+    </div>
+  )
+}
+
+export default Todo
