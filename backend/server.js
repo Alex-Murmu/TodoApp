5ec4b0aa9a3af4678083a3412bf0c7a630824a91
Alex Murmu <ft.alexrayen@gmial.com>
2025-03-10 02:47:24 +0000

    second commit

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,21 @@ const port = 2000;
 const app = express();
 app.use(express.json());
 
+// create todo
+app.post("/todo",function(req,res){
 
+})
+
+
+// get all todos 
+app.get("/todos",function(req,res){
+
+})
+
+// mark as completed
+app.put("/completed",function(req,res){
+    
+})
 
 app.listen(port,()=>{
     console.log("server is running")
